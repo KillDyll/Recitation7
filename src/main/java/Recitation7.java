2979d76d6419f62cd9cf76256a66eab8468736c4
dylandixon <djdixon22@students.desu.edu>
2023-11-15 23:23:12 -0500

    Recitation 7

--- src/main/java/Recitation7.java
+++ src/main/java/Recitation7.java
@@ -10,25 +10,42 @@ public class Recitation7
 {
     // define your constants after this comment
 
-
+    private static String generate(int num, char ch){
+        String result="";
+        for(int count=0; count < num; count ++){
+            result += ch;
+        }
+        return result;
+    }
     public static String leftRightTriangle(int height)
     {
         String str = "";
-        //add your code here
+        for(int tri = 1; tri  <= height; tri ++){
+            str += generate(tri, '*') +"\n";
+        }
         return str;
     }
 
     public static String rightRightTriangle(int height)
     {
         String str = "";
-        //add your code here
+        int righttri = height-1;
+        for(int tri = 1; tri  <= height; tri ++){
+            str += generate(righttri, ' ') + generate(tri, '*') +"\n";
+            righttri -= 1;
+        }
         return str;
     }
 
     public static String circle(int radius)
     {
         String str = "";
-        //add your code here
+
+        for(int rad = radius-1; rad>= -radius+1; rad --){
+            int x = (int)Math.sqrt(radius * radius-rad*rad);
+            str += generate(radius - x , ' ') + generate(2 * x, '*') +"\n";
+
+        }
         return str;
     }
 }
